8d2169fa9d622256dd14ffc66dedae8dcc161989
Harikrishnan-Ashok <harikrishnanashok2001@gmail.com>
2023-09-15 22:06:50 +0530

    undo1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const [turn, setTurn] = useState("");
-  function handleReset() {
-    setTurn("");
-  }
   return (
     <div className="app-container">
       <h1>x's turn</h1>
       <div className="tileBox">
-        {arr.map((el) => (
-          <Tile key={el}>{turn}</Tile>
-        ))}
+        <Tile></Tile>
+        <Tile></Tile>
+        <Tile></Tile>
+        <Tile></Tile>
+        <Tile></Tile>
+        <Tile></Tile>
+        <Tile></Tile>
+        <Tile></Tile>
+        <Tile></Tile>
       </div>
       <div className="button-container">
-        <Button onhandleReset={handleReset}>Reset</Button>
+        <Button>Reset</Button>
       </div>
     </div>
   );
 }
-function Tile({ children }) {
-  return <div className="Tile">{children}</div>;
+function Tile() {
+  return <div className="Tile"></div>;
 }
-function Button({ children, onhandleReset }) {
-  return (
-    <button className="customButton" onClick={onhandleReset}>
-      {children}
-    </button>
-  );
+function Button({ children }) {
+  return <button className="customButton">{children}</button>;
 }
