fe11711c8afd4d0b2f50160c34eb7e42a05fd0bc
Harikrishnan-Ashok <harikrishnanashok2001@gmail.com>
2023-09-15 23:34:20 +0530

    finally the states are good

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  const arr = [
+    { id: 1, value: null },
+    { id: 2, value: null },
+    { id: 3, value: null },
+    { id: 4, value: null },
+    { id: 5, value: null },
+    { id: 6, value: null },
+    { id: 7, value: null },
+    { id: 8, value: null },
+    { id: 9, value: null },
+  ];
+  const [turn, setTurn] = useState("x");
   return (
-    <div className="app-container">
-      <h1>x's turn</h1>
-      <div className="tileBox">
-        <Tile></Tile>
-        <Tile></Tile>
-        <Tile></Tile>
-        <Tile></Tile>
-        <Tile></Tile>
-        <Tile></Tile>
-        <Tile></Tile>
-        <Tile></Tile>
-        <Tile></Tile>
+    <>
+      <div className="app-container">
+        <h1 style={{ color: "white" }}>TIC-TAC-TOE</h1>
+        <h1>{turn}'s turn</h1>
+        <div className="tileBox">
+          {arr.map((el) => (
+            <Tile currTile={el} turn={turn} setturn={setTurn}></Tile>
+          ))}
+        </div>
+        <div className="button-container">
+          <Button>Reset</Button>
+        </div>
       </div>
-      <div className="button-container">
-        <Button>Reset</Button>
-      </div>
-    </div>
+    </>
   );
 }
-function Tile() {
-  return <div className="Tile"></div>;
+function Tile({ currTile, turn, setturn }) {
+  const [tileState, setTileState] = useState("");
+  function xplay() {
+    setTileState("❌");
+    setturn("o");
+  }
+  function oplay() {
+    setTileState("⭕");
+    setturn("x");
+  }
+  function handleonClick() {
+    tileState === "" && (turn === "x" ? xplay() : oplay());
+  }
+  return (
+    <div className="Tile" onClick={handleonClick}>
+      {tileState}
+    </div>
+  );
 }
 function Button({ children }) {
   return <button className="customButton">{children}</button>;
