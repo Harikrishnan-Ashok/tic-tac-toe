5695eed7c13951167b2a685e5966be0eee9d4e89
Harikrishnan-Ashok <harikrishnanashok2001@gmail.com>
2023-09-16 00:49:18 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import React, { useState } from "react";
 import "./App.css";
-import { useState } from "react";
 
 export default function App() {
-  const arr = [
+  const initialArr = [
     { id: 1, value: null },
     { id: 2, value: null },
     { id: 3, value: null },
@@ -13,43 +13,91 @@ export default function App() {
     { id: 8, value: null },
     { id: 9, value: null },
   ];
-  const [turn, setTurn] = useState("x");
+
+  const [arr, setArr] = useState(initialArr);
+  const [turn, setTurn] = useState("X");
+  const [winner, setWinner] = useState(null);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  };
+
+  const handleClick = (id) => {
+    if (winner || arr[id - 1]?.value) return;
+
+    const newArr = [...arr];
+    newArr[id - 1] = { ...newArr[id - 1], value: turn };
+    setArr(newArr);
+
+    const newTurn = turn === "X" ? "O" : "X";
+    setTurn(newTurn);
+
+    const result = calculateWinner(newArr.map((tile) => tile.value));
+    if (result) {
+      setWinner(result);
+    }
+  };
+
+  const resetGame = () => {
+    setArr(initialArr);
+    setTurn("X");
+    setWinner(null);
+  };
+
   return (
-    <>
-      <div className="app-container">
-        <h1 style={{ color: "white" }}>TIC-TAC-TOE</h1>
-        <h1>{turn}'s turn</h1>
-        <div className="tileBox">
-          {arr.map((el) => (
-            <Tile currTile={el} turn={turn} setturn={setTurn}></Tile>
-          ))}
-        </div>
-        <div className="button-container">
-          <Button>Reset</Button>
-        </div>
+    <div className="app-container">
+      <h1 style={{ color: "white" }}>TIC-TAC-TOE</h1>
+      <h1>{winner ? `Winner: ${winner}` : `${turn}'s turn`}</h1>
+      <div className="tileBox">
+        {arr.map((el) => (
+          <Tile
+            key={el.id}
+            currTile={el}
+            onClick={() => handleClick(el.id)}
+          ></Tile>
+        ))}
       </div>
-    </>
-  );
-}
-function Tile({ currTile, turn, setturn }) {
-  const [tileState, setTileState] = useState("");
-  function xplay() {
-    setTileState("❌");
-    setturn("o");
-  }
-  function oplay() {
-    setTileState("⭕");
-    setturn("x");
-  }
-  function handleonClick() {
-    tileState === "" && (turn === "x" ? xplay() : oplay());
-  }
-  return (
-    <div className="Tile" onClick={handleonClick}>
-      {tileState}
+      <div className="button-container">
+        <Button onClick={resetGame}>Reset</Button>
+      </div>
     </div>
   );
 }
-function Button({ children }) {
-  return <button className="customButton">{children}</button>;
+
+function Tile({ currTile, onClick }) {
+  return (
+    <div className="Tile" onClick={onClick}>
+      {currTile.value}
+    </div>
+  );
+}
+
+function Button({ children, onClick }) {
+  return (
+    <button className="customButton" onClick={onClick}>
+      {children}
+    </button>
+  );
 }
